c371d672575d2c4a9cddbb443fcde6e9550a5cde
Luca Bertaggia <luca.bertaggia82@gmail.com>
2024-12-18 21:16:58 +0100

    Add method documentation

--- FileManager/__init__.py
+++ FileManager/__init__.py
@@ -11,8 +11,19 @@ class File:
     def __init__(self, filename: str = ""):
         self.filename = filename
 
+    def get_filename(self) -> str:
+        """ Get file name """
+        return self.filename
+
     def rename(self, new_filename: str) -> None:
-        """ Change file name """
+        """
+        Rename the current file to a new filename\n
+        Args:
+            `new_filename (str)`: The new name for the file
+        Raises:
+            `FileNotFoundError`: If the current file does not exist
+            `FileExistsError`: If a file with the new filename already exists
+        """
         try:
             os.rename(self.filename, new_filename)
             self.filename = new_filename
@@ -22,32 +33,52 @@ class File:
             raise FileExistsError(f"File {new_filename} already exists")
 
     def exists(self) -> bool:
-        """ Check if file exists """
+        """
+        Check if file exists\n
+        Returns:
+            `bool`: True if file exists, False otherwise
+        """
         return os.path.exists(self.filename)
 
     def is_empty(self) -> bool:
-        """ Check if file is empty """
+        """
+        Check if file is empty\n
+        Returns:
+            `bool`: True if file is empty, False otherwise
+        """
         try:
             return os.path.getsize(self.filename) == 0
         except FileNotFoundError:
             raise FileNotFoundError(f"File {self.filename} not found")
 
     def create(self) -> None:
-        """ Create file """
+        """
+        Create the file\n
+        Raises:
+            `FileExistsError`: If the file already exists
+        """
         try:
             open(self.filename, "x").close()
         except FileExistsError:
             raise FileExistsError(f"File {self.filename} already exists")
 
     def delete(self) -> None:
-        """ Delete file """
+        """
+        Delete the file\n
+        Raises:
+            `FileNotFoundError`: If the file does not exist
+        """
         try:
             os.remove(self.filename)
         except FileNotFoundError:
             raise FileNotFoundError(f"File {self.filename} not found")
 
     def read(self) -> str:
-        """ Read file """
+        """
+        Read file content\n
+        Returns:
+            `str`: The file content
+        """
         try:
             with open(self.filename, "r") as file:
                 return file.read()
@@ -55,7 +86,11 @@ class File:
             raise FileNotFoundError(f"File {self.filename} not found")
 
     def readlines(self) -> list:
-        """ Read file lines """
+        """
+        Read file content\n
+        Returns:
+            `list`: A list containing all lines in the file
+        """
         try:
             with open(self.filename, "r") as file:
                 return file.readlines()
@@ -63,15 +98,31 @@ class File:
             raise FileNotFoundError(f"File {self.filename} not found")
 
     def write(self, text: str) -> int:
-        """ write in file """
+        """
+        Write text in file\n
+        Args:
+            `text (str)`: The text to write in the file
+        Returns:
+            `int`: 0 if file exists, 1 otherwise
+        """
         return self.__write(text, "w")
 
     def append(self, text: str) -> int:
-        """ Append text in file """
+        """
+        Write text in file in append mode\n
+        Args:
+            `text (str)`: The text to append in the file
+        Returns:
+            `int`: 0 if file exists, 1 otherwise
+        """
         return self.__write(text, "a")
 
     def clear(self) -> None:
-        """ Remove all text in the file """
+        """
+        Remove all text in the file\n
+        Raises:
+            `FileNotFoundError`: If the file does not exist
+        """
         if (not self.exists()):
             raise FileNotFoundError(f"File {self.filename} not found")
         with open(self.filename, "w") as file:
